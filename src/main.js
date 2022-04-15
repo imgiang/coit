167c8ddc11303966de58849a158713f89b227c2e
imgiang <hoahonggai1199@gmail.com>
2022-04-15 15:24:41 +0700

    .

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+// firebase
+// import firebase from "firebase/app";
+
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
@@ -23,6 +26,18 @@ Vue.use(IconsPlugin);
 library.add(faSpinner);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
+// firebase.initializeApp({
+//   apiKey: "AIzaSyAy094WT_V6fRjfBbUI1j520U7JCvRBfgU",
+//   authDomain: "fir-firebase-562a6.firebaseapp.com",
+//   databaseURL:
+//     "https://fir-firebase-562a6-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "fir-firebase-562a6",
+//   storageBucket: "fir-firebase-562a6.appspot.com",
+//   messagingSenderId: "730620184377",
+//   appId: "1:730620184377:web:340fdb1cd8a92cff360ae8",
+//   measurementId: "G-XKD5DG5P7X",
+// });
+
 new Vue({
   router,
   store,
@@ -30,3 +45,8 @@ new Vue({
     return h(App);
   },
 }).$mount("#app");
+
+// new Vue({
+//   router,
+//   render: (h) => h(App),
+// }).$mount("#app");
